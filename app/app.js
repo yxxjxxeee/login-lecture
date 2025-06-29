8a73367f2e0e1527b75395acf40146dfe4323a8b
YJE <c0d1ngyje@gmail.com>
2025-06-29 22:43:49 +0900

    깃 저장소 초기화

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+// 미들 웨어를 등록해주는 메서드
+app.use("/", home); 
+
+module.exports = app;
---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,15 @@
+// 컨트롤러
+"use strict";
+
+const hello = (req, res) => {
+  res.render("home/index");
+};
+
+const login = (req, res) => {
+  res.render("home/login");
+};
+
+module.exports = {
+  hello,
+  login,
+};
