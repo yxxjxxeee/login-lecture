7a2b40da221ba18df2e5a6da30f7e4b9c8b41ced
YJE <c0d1ngyje@gmail.com>
2025-07-01 14:18:47 +0900

    백엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -1,18 +1,22 @@
 "use strict";
 
-// 모듈
+// express로 서버 생성하기
 const express = require("express");
 const app = express();
 
-// 라우팅
+const bodyParser = require("body-parser");
+
+// 라우팅 파일 가져오기(index.js)
 const home = require("./src/routes/home");
 
-// 앱 세팅
+// 앱 세팅(ejs 사용)
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-// 미들 웨어를 등록해주는 메서드
-app.use("/", home);
+// 미들 웨어 설정
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home);
 
 module.exports = app;
