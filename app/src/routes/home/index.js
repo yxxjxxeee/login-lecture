bf45eed7d8520e7a54489a6a9acab0bc9b6913f0
YJE <c0d1ngyje@gmail.com>
2025-07-02 14:11:53 +0900

    UserStorage 모델 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,7 +8,6 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
-
 router.post("/login", ctrl.process.login);
 
 module.exports = router;
