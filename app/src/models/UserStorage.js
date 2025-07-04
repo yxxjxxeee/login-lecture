a6bc2d865b5951a9cec06431bde9781905b5ad4c
YJE <c0d1ngyje@gmail.com>
2025-07-04 11:40:52 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -11,15 +11,27 @@ class UserStorage {
 
   static getUsers(...fields) {
     const users = this.#users;
+
     const newUsers = fields.reduce((newUsers, field) => {
+      // newUsers에 field 저장하기
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
       }
       return newUsers;
     }, {});
 
-    console.log(newUsers);
     return newUsers;
+  }
+
+  static getUserInfo(id) {
+    const users = this.#users;
+    const idx = users.id.indexOf(id);
+    const userInfo = Object.keys(users).reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
+
+    return userInfo;
   }
 }
 
