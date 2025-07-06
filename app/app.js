b1110e87d1f1253e930ca8a5b3319e9a28fb5462
YJE <c0d1ngyje@gmail.com>
2025-07-06 21:16:27 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
-// express로 서버 생성하기
+// 모듈
 const express = require("express");
-const app = express();
-
+const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+
+dotenv.config();
+const app = express();
 
 // 라우팅 파일 가져오기(index.js)
 const home = require("./src/routes/home");
@@ -15,7 +17,7 @@ app.set("view engine", "ejs");
 
 // 미들 웨어 설정
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/src/public`));
 app.use("/", home);
 
