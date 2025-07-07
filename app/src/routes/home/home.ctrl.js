1398c133b1d8ab45009a9188acbfcc09852469dd
YJE <c0d1ngyje@gmail.com>
2025-07-07 23:59:11 +0900

    최종 최적화

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,15 +6,17 @@ const logger = require("../../config/logger");
 
 const output = {
   home: (req, res) => {
-    logger.info(`GET / 200 "홈 화면으로 이동"`);
+    logger.info(`GET / 304 "홈 화면으로 이동"`);
     res.render("home/index");
   },
+
   login: (req, res) => {
-    logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+    logger.info(`GET /login 304 "로그인 화면으로 이동"`);
     res.render("home/login");
   },
+
   register: (req, res) => {
-    logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+    logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
     res.render("home/register");
   }
 };
@@ -22,24 +24,41 @@ const output = {
 const process = {
   login: async (req, res) => {
     const user = new User(req.body);
-    const responce = await user.login();
-    if (responce.err)
-      logger.error(`POST /login 200 Response: "success: ${responce.success}, ${responce.err}"`);
-    else
-      logger.info(`POST /login 200 Response: "success: ${responce.success}, msg: ${responce.msg}"`);
-    return res.json(responce);
+    const response = await user.login();
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 
   register: async (req, res) => {
     const user = new User(req.body);
-    const responce = await user.register();
-    if (responce.err)
-      logger.error(`POST /login 200 Response: "success: ${responce.success}, ${responce.err}"`);
-    else
-      logger.info(`POST /login 200 Response: "success: ${responce.success}, msg: ${responce.msg}"`);
-    return res.json(responce);
+    const response = await user.register();
+
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 500 : 201
+    };
+
+    log(response, url);
+    return res.status(url.status).json(response);
   }
 };
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(`${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`);
+  }
+  else {
+    logger.info(`${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg || ""}`);
+  }
+}
 
 module.exports = {
   output,
