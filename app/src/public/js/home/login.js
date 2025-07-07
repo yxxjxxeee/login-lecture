62d0bcadc1ab0f2b6f06548b645837d9408633e8
YJE <c0d1ngyje@gmail.com>
2025-07-07 23:23:03 +0900

    winston 프로젝트에 적용

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,6 +7,9 @@ const id = document.querySelector("#id"),
 loginBtn.addEventListener("click", login);
 
 function login() {
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+  if (!psword.value) return alert("비밀번호를 입력해주십시오.");
+
   // req 객체에 id, pwword 값 담기
   const req = {
     id: id.value,
@@ -28,6 +31,7 @@ function login() {
         location.href = "/";
       }
       else {
+        if (res.err) return alert(res.err);
         alert(res.msg);
       }
     })
