73591b8a8274fb83d2e5f73beb6a6b64dbc04afa
YJE <c0d1ngyje@gmail.com>
2025-07-08 15:28:11 +0900

    port 번호 변경

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const app = require("../app");
+const dotenv = require("dotenv");
 const logger = require("../src/config/logger");
-const PORT = 3000;
+const PORT = process.env.PORT | 3000;
 
 app.listen(PORT, () => {
     logger.info(`${PORT} 포트에서 서버가 가동되었습니다.`);
